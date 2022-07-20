5342e4f4b2aca254c937c2b270b917c9796385af
ishanwardhono <ishanwardhono@gmail.com>
2022-07-20 15:07:55 +0700

    cuma rearrange mod aja

--- src/cores/database/mod.rs
+++ src/cores/database/mod.rs
@@ -1,7 +1,7 @@
+pub mod schema;
+
 use diesel::prelude::*;
 use diesel::r2d2::{self, ConnectionManager};
-
-pub mod schema;
 
 pub type Pool = r2d2::Pool<ConnectionManager<PgConnection>>;
 
--- src/services/state/mod.rs
+++ src/services/state/mod.rs
@@ -1,9 +1,8 @@
-use actix_web::{web, get, Responder};
-
-use crate::cores::database;
-
 mod model;
 mod repo;
+
+use actix_web::{web, get, Responder};
+use crate::cores::database;
 
 #[get("/state/register")]
 pub async fn get_states(data: web::Data<database::Pool>) -> impl Responder  {
