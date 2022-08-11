b100bcd119daf3c4261aede2b0795231264955ee
ishanwardhono-ps <muhammad.ishan.wardhono@pingspace.co>
2022-08-11 14:41:51 +0800

    fix the data ddl

--- data/ddl.sql
+++ data/ddl.sql
@@ -1,9 +1,7 @@
--- Your SQL goes here
-
 CREATE TABLE states (
     id SERIAL NOT NULL PRIMARY KEY,
     code VARCHAR(20) NOT NULL,
     description TEXT,
     webhooks TEXT[],
-    created_at TIMESTAMP NOT NULL
+    created_at TIMESTAMP NOT NULL DEFAULT (now() at time zone 'utc')
 );
